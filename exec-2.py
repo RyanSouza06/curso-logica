c4a39076daca1f61913d018226b3bdc4621493a2
RyanSouza06 <ryansouza04.2006@gmail.com>
2024-09-02 18:56:39 -0300

    Update exec-2.py

--- exec-2.py
+++ exec-2.py
@@ -1,5 +1,9 @@
 #Elabore um programa que imprimaseu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira#
 
-print('Ryan')
-print('RJ')
-print('Rio de Janeiro')
+print('etiqueta')
+nome = str(input('Seu nome: '))
+estado = str(input('Seu estado: '))
+municipio = str(input('Seu município: '))
+print(nome)
+print(estado)
+print(municipio)
