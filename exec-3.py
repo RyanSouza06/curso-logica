7dc893c6d45fbff1d9a59b9114a7c3772ad99fc2
RyanSouza06 <ryansouza04.2006@gmail.com>
2024-09-02 19:00:01 -0300

    Create exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,3 @@
+ #Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber).#
+musica = str(input('A música que você mais gosta: '))
+print(musica,'é a sua música favorita!')
